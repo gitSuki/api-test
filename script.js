c319dccc47d95ba84999a3d09426488a7f746f72
gitSuki <collinbrown226@gmail.com>
2022-07-09 06:46:34 -0500

    Complete fetch request and adjust image SRC based on fetch

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const API_KEY = 'U1AcoV0LydlsfjQH4T72RS3cgccXjGh2'
-const img = document.querySelector('img')
+const img = document.querySelector("img")
 
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=cats`, {mode: `cors`}).then(function(response) {
-    console.log(response.json())
-})
+fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=cats`, {mode: `cors`})
+    .then( response => {
+        // the fetch request returns a promise
+        console.log(response)
+        return response.json()
+    }).then( response => {
+        console.log(response) // logs the response object
+        console.log(response.data.images.original.url) // logs the relevant url from the object
+        img.src = response.data.images.original.url
+    })
